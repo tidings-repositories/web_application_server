8d1f4bf85be1c1044404280a2ded96d5b345fb04
JYKIM317 <augu202@naver.com>
2025-06-12 10:07:50 +0900

    chore: project initialize setting and builder

---
+++ app/src/test/java/com/delivalue/tidings/TidingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivalue.tidings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
