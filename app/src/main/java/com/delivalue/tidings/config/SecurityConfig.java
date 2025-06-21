541d9e1638175e15f579741a6a4fd30fd86b5354
JYKIM317 <augu202@naver.com>
2025-06-22 08:05:22 +0900

    feat: Add Spring Security Filter iptions
    1. POST, DELETE, PUT 등의 안전하지 않은 HTTP 요청의 프리플라이트에 요청에 대한 정상적인 응답을 위해 OPTIONS 메서드를 허용하도록 추가하였습니다.
    
    2. Spring Security Filter chain에서 CSRF 토큰을 이용하지 않도록 변경하였습니다 (서비스 내 Access 및 Refresh 토큰을 이용할 것이기 때문)
    
    3. /auth 컨트롤러에 접근하는 모든 요청에 대하여 openID 인증 및 세션에 사용자 정보가 있는지 검증하도록 옵션을 추가하였습니다.

--- app/src/main/java/com/delivalue/tidings/config/SecurityConfig.java
+++ app/src/main/java/com/delivalue/tidings/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.delivalue.tidings.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
@@ -17,8 +20,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/oauth2/authorization/**").authenticated()
+                        .requestMatchers("/auth/**").authenticated()
                         .anyRequest().permitAll()
                 )
                 .oauth2Login((login) -> login
@@ -28,14 +33,19 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    public FilterRegistrationBean<CorsFilter> corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of(("http://localhost:5173")));
-        configuration.setAllowedMethods(List.of("GET"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
 
-        return source;
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return bean;
     }
 }
--- app/src/main/java/com/delivalue/tidings/config/WebConfig.java
+++ app/src/main/java/com/delivalue/tidings/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
     }
