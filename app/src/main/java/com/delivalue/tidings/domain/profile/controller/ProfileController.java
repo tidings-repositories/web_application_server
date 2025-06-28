934ab950bb4ab6fac82c153876f32fe9fb3aa607
JYKIM317 <augu202@naver.com>
2025-06-29 04:34:01 +0900

    refactor: Remove unnecessary import
    사용되지 않는, 불필요한 import 코드를 제거하였습니다.

--- app/src/main/java/com/delivalue/tidings/domain/profile/controller/ProfileController.java
+++ app/src/main/java/com/delivalue/tidings/domain/profile/controller/ProfileController.java
@@ -8,11 +8,9 @@ import com.delivalue.tidings.domain.profile.dto.ProfileUpdateRequest;
 import com.delivalue.tidings.domain.profile.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
