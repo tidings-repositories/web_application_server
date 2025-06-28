90a4fde09462248ec45adab9f5e0d10f01c01778
JYKIM317 <augu202@naver.com>
2025-06-28 22:26:44 +0900

    Feat: Add using QueryDSL Repository
    QueryDSL을 이용하여 동적 쿼리를 생성하여 프로필에 접근하는 Repository를 구현하였습니다.

---
+++ app/src/main/java/com/delivalue/tidings/domain/data/repository/MemberQueryRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.delivalue.tidings.domain.data.repository;
+
+import com.delivalue.tidings.domain.data.entity.QMember;
+import com.delivalue.tidings.domain.profile.dto.ProfileUpdateRequest;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.impl.JPAUpdateClause;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberQueryRepositoryImpl implements MemberQueryRepository{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    @Transactional
+    public long updateMemberProfile(ProfileUpdateRequest request) {
+        boolean hasUpdate = false;
+        QMember qMember = QMember.member;
+
+        JPAUpdateClause update = queryFactory.update(qMember);
+
+        if(request.getUserName() != null) {
+            update.set(qMember.name, request.getUserName());
+            hasUpdate = true;
+        }
+
+        if(request.getProfileImage() != null) {
+            update.set(qMember.profileImage, request.getProfileImage());
+            hasUpdate = true;
+        }
+
+        if(request.getBio() != null) {
+            update.set(qMember.bio, request.getBio());
+            hasUpdate = true;
+        }
+
+        if(request.getBadgeId() != null) {
+            update.set(qMember.badge.id, request.getBadgeId());
+            hasUpdate = true;
+        }
+
+        return hasUpdate ? update.where(qMember.id.eq(request.getId())).execute() : 0;
+    }
+}
