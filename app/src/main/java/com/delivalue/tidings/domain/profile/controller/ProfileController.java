7f8a7e4b459738baaeaa8f62be83300df8e2cb73
JYKIM317 <augu202@naver.com>
2025-07-05 07:18:49 +0900

    feat: Add get user like post list API
    1. feat: 특정 사용자가 좋아요 표시한 포스트 리스트를 반환하는 Controller API를 정의하였습니다.
    
    2. refactor: 기존 PostController에서 관리되던 프로필 페이지 접근 시 해당 유저가 발행한 포스트를 반환하는 접근 API를 PostController에서 ProfileController로 책임을 변경하였습니다.

--- app/src/main/java/com/delivalue/tidings/domain/profile/controller/ProfileController.java
+++ app/src/main/java/com/delivalue/tidings/domain/profile/controller/ProfileController.java
@@ -4,6 +4,8 @@ import com.delivalue.tidings.common.RequestValidator;
 import com.delivalue.tidings.common.TokenProvider;
 import com.delivalue.tidings.domain.data.repository.FollowRepository;
 import com.delivalue.tidings.domain.follow.service.FollowService;
+import com.delivalue.tidings.domain.post.dto.PostResponse;
+import com.delivalue.tidings.domain.post.service.PostService;
 import com.delivalue.tidings.domain.profile.dto.BadgeListResponse;
 import com.delivalue.tidings.domain.profile.dto.ProfileResponse;
 import com.delivalue.tidings.domain.profile.dto.ProfileUpdateRequest;
@@ -13,6 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +26,7 @@ import java.util.Map;
 public class ProfileController {
     private final ProfileService profileService;
     private final FollowService followService;
+    private final PostService postService;
     private final TokenProvider tokenProvider;
     private final RequestValidator requestValidator;
 
@@ -125,5 +130,34 @@ public class ProfileController {
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
+    }
+
+    @PostMapping("/{publicId}/posts")
+    public ResponseEntity<List<PostResponse>> requestUserPostList(@PathVariable("publicId") String publicId, @RequestBody Map<String, String> body) {
+        OffsetDateTime requestCursor = body.get("createdAt") != null ? OffsetDateTime.parse((String) body.get("createdAt")) : null;
+        if(publicId == null || requestCursor == null) return ResponseEntity.badRequest().build();
+        LocalDateTime cursorTime = requestCursor.toLocalDateTime();
+
+        System.out.printf("this time is: " + cursorTime.toString());
+
+        try {
+            List<PostResponse> result = this.postService.getUserPostByCursor(publicId, cursorTime);
+
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            System.out.printf("Catch Error /post/{publicId}: " + e.getMessage());
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("/{publicId}/likes")
+    public ResponseEntity<List<PostResponse>> requestUserLikePost(@PathVariable("publicId") String publicId, @RequestBody Map<String, String> body) {
+        OffsetDateTime requestCursor = body.get("createdAt") != null ? OffsetDateTime.parse((String) body.get("createdAt")) : null;
+        if(publicId == null || requestCursor == null) return ResponseEntity.badRequest().build();
+        LocalDateTime cursorTime = requestCursor.toLocalDateTime();
+        String cursorId = body.get("postId");
+
+        List<PostResponse> result = this.postService.getUserLikePostByCursor(publicId, cursorTime, cursorId);
+        return ResponseEntity.ok(result);
     }
 }
