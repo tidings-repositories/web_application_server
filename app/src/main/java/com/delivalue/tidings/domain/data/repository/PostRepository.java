0a97a80c6eba7d0b1fe42b241d790fa86edb96b1
JYKIM317 <augu202@naver.com>
2025-07-05 07:14:24 +0900

    feat: Add PostRepository get find List data
    PostRepository에 키 리스트를 통해 값 리스트를 반환하는 메서드를 정의해줬습니다.

--- app/src/main/java/com/delivalue/tidings/domain/data/repository/PostRepository.java
+++ app/src/main/java/com/delivalue/tidings/domain/data/repository/PostRepository.java
@@ -4,9 +4,12 @@ import com.delivalue.tidings.domain.data.entity.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     Optional<Post> findByIdAndDeletedAtIsNull(String id);
+
+    List<Post> findByIdInAndDeletedAtIsNull(List<String> ids);
 }
