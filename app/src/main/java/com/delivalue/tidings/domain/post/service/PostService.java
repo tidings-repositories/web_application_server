e92aabad83662662a71a6e28fc27547eb149ddf0
JYKIM317 <augu202@naver.com>
2025-07-05 07:15:52 +0900

    feat: Add Post like service logic
    포스트의 상호작용 중 좋아요 기능이 동작할 수 있도록 like, unlike 및 특정 유저가 좋아요를 표현한 포스트를 반환하는 메서드를 구현하였습니ㅏㄷ.

--- app/src/main/java/com/delivalue/tidings/domain/post/service/PostService.java
+++ app/src/main/java/com/delivalue/tidings/domain/post/service/PostService.java
@@ -1,9 +1,11 @@
 package com.delivalue.tidings.domain.post.service;
 
 import com.delivalue.tidings.domain.data.entity.Badge;
+import com.delivalue.tidings.domain.data.entity.Like;
 import com.delivalue.tidings.domain.data.entity.Member;
 import com.delivalue.tidings.domain.data.entity.Post;
 import com.delivalue.tidings.domain.data.repository.MemberRepository;
+import com.delivalue.tidings.domain.data.repository.PostLikeRepository;
 import com.delivalue.tidings.domain.data.repository.PostRepository;
 import com.delivalue.tidings.domain.post.dto.PostCreateRequest;
 import com.delivalue.tidings.domain.post.dto.PostResponse;
@@ -12,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -30,6 +33,7 @@ public class PostService {
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
     private final MongoTemplate mongoTemplate;
+    private final PostLikeRepository postLikeRepository;
 
     public PostResponse getPostByPostId(String postId) {
         Optional<Post> result = this.postRepository.findByIdAndDeletedAtIsNull(postId);
@@ -121,5 +125,86 @@ public class PostService {
 
         post.setDeletedAt(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
         this.postRepository.save(post);
+    }
+
+    public void likePost(String internalId, String postId) {
+        //Add Like document
+        Optional<Member> likeMember = this.memberRepository.findById(internalId);
+        if(likeMember.isEmpty()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        Member member = likeMember.get();
+
+        Optional<Post> likePost = this.postRepository.findById(postId);
+        if(likePost.isEmpty()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        Post post = likePost.get();
+        if(post.getInternalUserId().equals(internalId)) throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+
+        Optional<Like> like = this.postLikeRepository.findByLikeUserIdAndPostId(member.getPublicId(), postId);
+        if(like.isPresent()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        Like newLikeEntity = Like.builder()
+                .likeUserId(member.getPublicId())
+                .postId(postId)
+                .postCreatedAt(post.getCreatedAt())
+                .likeAt(LocalDateTime.now(ZoneId.of("Asia/Seoul"))).build();
+
+        this.postLikeRepository.save(newLikeEntity);
+
+        try {
+            //Increase Post document like count
+            Query query = Query.query(Criteria.where("_id").is(post.getId()));
+            Update update = new Update().inc("likeCount", 1);
+            this.mongoTemplate.updateFirst(query, update, Post.class);
+        } catch (Exception e) {
+            this.postLikeRepository.delete(newLikeEntity);
+        }
+    }
+
+    public void unlikePost(String internalId, String postId) {
+        //Remove Like document
+        Optional<Member> unlikeMember = this.memberRepository.findById(internalId);
+        if(unlikeMember.isEmpty()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        Member member = unlikeMember.get();
+
+        Optional<Like> like = this.postLikeRepository.findByLikeUserIdAndPostId(member.getPublicId(), postId);
+        if(like.isEmpty()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        Like likeEntity = like.get();
+        this.postLikeRepository.delete(likeEntity);
+
+        try {
+            //Decrease Post document like count
+            Query query = Query.query(Criteria.where("_id").is(likeEntity.getPostId()));
+            Update update = new Update().inc("likeCount", -1);
+            this.mongoTemplate.updateFirst(query, update, Post.class);
+        } catch (Exception e) {
+            this.postLikeRepository.save(likeEntity);
+        }
+    }
+
+    public List<PostResponse> getUserLikePostByCursor(String userId, LocalDateTime cursorTime, String cursorId) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("likeUserId").is(userId));
+
+        if(cursorId != null) {
+            query.addCriteria(
+                    new Criteria().orOperator(
+                            Criteria.where("postCreatedAt").lt(cursorTime),
+                            new Criteria().andOperator(
+                                    Criteria.where("postCreatedAt").is(cursorTime),
+                                    Criteria.where("postId").lt(cursorId)
+                            )
+                    )
+            );
+        }
+
+        query.with(Sort.by(Sort.Direction.DESC, "postCreatedAt"));
+        query.limit(15);
+
+        List<Like> likeList = this.mongoTemplate.find(query, Like.class);
+        List<String> postIdList = likeList.stream().map(Like::getPostId).toList();
+        List<Post> likePostList = this.postRepository.findByIdInAndDeletedAtIsNull(postIdList);
+
+        return likePostList.stream().map(PostResponse::new).collect(Collectors.toList());
     }
 }
