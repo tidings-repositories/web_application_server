808b16531ea4dff143a252ccc722aceb153d948d
JYKIM317 <augu202@naver.com>
2025-07-09 16:51:45 +0900

    refactor: Add search condition
    유저 검색 과정에서 keyword가 null이거나 2 글자 미만인 검색을 수행하지 않도록 조건을 추가하였습니다.

--- app/src/main/java/com/delivalue/tidings/domain/search/controller/SearchController.java
+++ app/src/main/java/com/delivalue/tidings/domain/search/controller/SearchController.java
@@ -2,12 +2,12 @@ package com.delivalue.tidings.domain.search.controller;
 
 import com.delivalue.tidings.common.TokenProvider;
 import com.delivalue.tidings.domain.profile.dto.ProfileResponse;
-import com.delivalue.tidings.domain.profile.service.ProfileService;
 import com.delivalue.tidings.domain.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -22,10 +22,10 @@ public class SearchController {
     public ResponseEntity<List<ProfileResponse>> requestSearchUser(@RequestHeader("Authorization") String authorizationHeader, @RequestParam(value = "q") String keyword) {
         int TOKEN_PREFIX_LENGTH = 7;
 
+        if(keyword == null || keyword.length() < 2) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         if(authorizationHeader != null
                 && authorizationHeader.startsWith("Bearer ")
                 && this.tokenProvider.validate(authorizationHeader.substring(TOKEN_PREFIX_LENGTH))) {
-            String id = this.tokenProvider.getUserId(authorizationHeader.substring(TOKEN_PREFIX_LENGTH));
 
             List<ProfileResponse> result = this.searchService.getProfileBySearchKeyword(keyword);
 
