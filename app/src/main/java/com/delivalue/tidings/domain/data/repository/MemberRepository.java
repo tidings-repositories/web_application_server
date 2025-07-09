a91f5fca6a80b472764cc9f7deab33ca6bd21405
JYKIM317 <augu202@naver.com>
2025-07-09 16:34:16 +0900

    feat: Add feature search user
    유저를 검색할 수 있도록 컨트롤러 API와 서비스로직, Repository의 JPQL 메서드를 정의하였습니다.

--- app/src/main/java/com/delivalue/tidings/domain/data/repository/MemberRepository.java
+++ app/src/main/java/com/delivalue/tidings/domain/data/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -31,4 +32,11 @@ public interface MemberRepository extends JpaRepository<Member, String> {
     @Modifying
     @Query("UPDATE Member m SET m.followerCount = m.followerCount - 1 WHERE m.id = :id")
     void decreaseFollowerCount(@Param("id") String id);
+
+    @Query("""
+    SELECT m FROM Member m
+    WHERE (m.name LIKE CONCAT(:keyword, '%') OR m.publicId LIKE CONCAT(:keyword, '%'))
+    AND m.deletedAt IS NULL
+    """)
+    List<Member> searchByKeyword(@Param("keyword") String keyword);
 }
