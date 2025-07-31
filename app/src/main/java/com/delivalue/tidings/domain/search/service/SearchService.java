954a873a57f0253d12690f955e28de4312bfcfb2
JYKIM317 <augu202@naver.com>
2025-07-31 20:30:01 +0900

    feat: Add Post search logic and refactoring member search logic
    - PostSearchRepository를 이용해 포스트를 검색하고 반환하는 getPostBySearchKeyword 함수를 추가하였습니다.
    
    - 기존 MySQL DB에서 LIKE를 통해 검색을 수행하던 Member 검색 로직을 Elasticsearch를 통한 검색 로직으로 변경하였고, 중간 과정에서 Badge 존재 여부에 따라 MySQL Badge 테이블을 조회할 수 있도록 추가하였습니다. (Trnasactional은 read only를 적용하여 영속성 컨텍스트를 통한 DB I/O를 줄이자는 취지입니다.)

--- app/src/main/java/com/delivalue/tidings/domain/search/service/SearchService.java
+++ app/src/main/java/com/delivalue/tidings/domain/search/service/SearchService.java
@@ -1,22 +1,56 @@
 package com.delivalue.tidings.domain.search.service;
 
+import com.delivalue.tidings.domain.data.entity.Badge;
 import com.delivalue.tidings.domain.data.entity.Member;
+import com.delivalue.tidings.domain.data.entity.MemberSearch;
+import com.delivalue.tidings.domain.data.entity.PostSearch;
+import com.delivalue.tidings.domain.data.repository.BadgeRepository;
 import com.delivalue.tidings.domain.data.repository.MemberRepository;
+import com.delivalue.tidings.domain.data.repository.MemberSearchRepository;
+import com.delivalue.tidings.domain.data.repository.PostSearchRepository;
+import com.delivalue.tidings.domain.post.dto.PostResponse;
+import com.delivalue.tidings.domain.profile.dto.BadgeDto;
 import com.delivalue.tidings.domain.profile.dto.ProfileResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
-    private final MemberRepository memberRepository;
+    private final PostSearchRepository postSearchRepository;
+    private final MemberSearchRepository memberSearchRepository;
+    private final BadgeRepository badgeRepository;
 
+    private final Pageable pageable = PageRequest.of(0, 30);
+
+    @Transactional(readOnly = true)
     public List<ProfileResponse> getProfileBySearchKeyword(String keyword) {
-        List<Member> searchResult = this.memberRepository.searchByKeyword(keyword);
+        List<MemberSearch> searchResult = this.memberSearchRepository.searchMembers(keyword, pageable);
 
-        return searchResult.stream().map(ProfileResponse::new).collect(Collectors.toList());
+
+        return searchResult.stream().map(result -> {
+            Integer badgeId = result.getBadge();
+
+            if(badgeId != null) {
+                Optional<Badge> targetBadge = this.badgeRepository.findById(badgeId);
+
+                BadgeDto badge = targetBadge.map(BadgeDto::new).orElse(null);
+                return new ProfileResponse(result, badge);
+            }
+
+            return new ProfileResponse(result, null);
+        }).toList();
+    }
+
+    public List<PostResponse> getPostBySearchKeyword(String keyword) {
+        List<PostSearch> searchResult = this.postSearchRepository.searchPosts(keyword, pageable);
+
+        return searchResult.stream().map(PostResponse::new).toList();
     }
 }
